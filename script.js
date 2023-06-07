3d02231fcbe01b5a0846c69503e7d487743b3c89
Vinod Ramdin <digikettechadmin@digikettech.com>
2023-06-07 01:00:09 -0500

    added in final touches

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-var clicks = 0;
-function myFunction() {
-    clicks+=1;
-    document.getElementById("clicks").innerHTML = clicks;
-}
-
 $(document).ready(function() {
-    $('#check').change(function() {
-      if($(this).is(":checked")) {
-        $('#credit_card_section').show();
-      } else {
-        $('#credit_card_section').hide();
-      }
-    });
- 
-
-    var clicks = 0;
-    function myFunction() {
-        clicks+=1;
-        document.getElementById("clicks").innerHTML = clicks;
+  $('#check').change(function() {
+    if ($(this).is(":checked")) {
+      $('#credit_card_section').show();
+    } else {
+      $('#credit_card_section').hide();
     }
+  });
 
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 2500, function() {
+        window.location.hash = hash;
+      });
+    }
+  });
 
+  const slideButtons = document.querySelectorAll('.slide-button');
+  const slideText = document.getElementById('slideText');
 
-
-
-
-
+  slideButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      slideText.classList.toggle('slide-in');
+    });
+  });
 });
